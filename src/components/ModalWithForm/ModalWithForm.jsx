8edef668cd81416a46c45e193785d55b287877ef
cdubb919 <christinanw919@gmail.com>
2025-09-16 05:09:15 -0500

    form created

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -9,7 +9,7 @@ function ModalWithForm() {
                     CLOSE
                     </button>
                     <label htmlFor="name" className="modal__label">
-                        Name(" ")
+                        Name{" "}
                         <input 
                         type="text" 
                         className="modal__input"
@@ -17,7 +17,34 @@ function ModalWithForm() {
                         placeholder="Name" 
                         />
                     </label>
-                </form>;
+                    <label htmlFor="imageUrl" className="modal__label">
+                        Image{" "}
+                        <input 
+                        type="url" 
+                        className="modal__input"
+                        id="imageUrl"
+                        placeholder="Image URL" 
+                        />
+                    </label>
+                    <fieldset className="modal__radio-buttons">
+                        <legend className="modal__legend">Select the weather type:</legend>
+                        <label htmlFor="hot" className="modal__label modal__label_type_radio">
+                            <input id="cold" type="radio" className="modal__radio-input"/>Hot
+                        </label>
+                        <label 
+                        htmlFor="warm" 
+                        className="modal__label modal__label_type_radio"
+                        >
+                        <input id="cold" type="radio" className="modal__radio-input" />Warm  
+                        </label>
+                        <label 
+                            htmlFor="cold"
+                            className="modal__label modal__label_type_radio"
+                            >
+                                <input id="cold" type="radio" className="modal__radio-input" />Cold
+                            </label>
+                    </fieldset>
+                </form>
             </div>
     );
 }
