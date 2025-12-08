a50a2a52882c35237da53f5a234ac6181fed1810
cdubb919 <christinanw919@gmail.com>
2025-12-08 09:36:20 -0600

    project 15 successfully deployed and running on browser

--- src/utils/apiHelpers.js
+++ src/utils/apiHelpers.js
@@ -1,4 +1,8 @@
-export const BASE_URL = "http://localhost:3001";
+export const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "api.wtwrcdubb.localghost.org"  
+    : "http://localhost:3001";  
+
 
 export const handleResponse = (res) => {
   if (res.ok) return res.json();
